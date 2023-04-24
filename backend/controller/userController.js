1c8626c02abcffaf9e053071a5032ccf3f2ff3ef
Rohit <90600620+rohit2k3@users.noreply.github.com>
2023-04-24 19:42:05 +0530

    fixed cookie issue

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -38,10 +38,12 @@ exports.loginUser = asyncError(async (req,res,next) => {
     const jwttoken = loginData.generateJWT();
 
     res.status(200).cookie("token",jwttoken,{
-        HttpOnly:true,
         expiresIn: new Date(
             Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000
         ),
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
     }).json({
         status:true,
         message:"Login Successfully",
